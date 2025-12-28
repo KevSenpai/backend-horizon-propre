80b29f003f56450fed8518605ffe67ba28cbb5cd
Kevin SEMITEJA <kevinsemiteja5@gmai.com>
2025-12-28 17:48:49 +0100

    Fix: Make vehicle status optional in DTO

--- src/vehicles/dto/create-vehicle.dto.ts
+++ src/vehicles/dto/create-vehicle.dto.ts
@@ -1,1 +1,16 @@
-export class CreateVehicleDto {}
+import { IsString, IsNotEmpty, IsEnum, IsOptional } from 'class-validator';
+import { VehicleStatus } from '../entities/vehicle.entity';
+
+export class CreateVehicleDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  license_plate: string;
+
+  @IsEnum(VehicleStatus)
+  @IsOptional() // <--- AJOUT IMPORTANT : Le statut est optionnel (défaut: OPERATIONAL)
+  status?: VehicleStatus;
+}
