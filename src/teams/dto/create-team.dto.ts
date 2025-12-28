b1d9b4fd74c5b9fdbea794aeb5d1777130f6a8cf
Kevin SEMITEJA <kevinsemiteja5@gmai.com>
2025-12-28 17:41:16 +0100

    Fix: Chart container dimensions

--- src/teams/dto/create-team.dto.ts
+++ src/teams/dto/create-team.dto.ts
@@ -1,1 +1,16 @@
-export class CreateTeamDto {}
+import { IsString, IsNotEmpty, IsEnum, IsOptional } from 'class-validator';
+import { TeamStatus } from '../entities/team.entity';
+
+export class CreateTeamDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsOptional() // <--- AJOUT IMPORTANT : Ce champ n'est pas obligatoire
+  @IsString()
+  members_info?: string;
+
+  @IsEnum(TeamStatus)
+  @IsOptional() // Le statut est optionnel car il a une valeur par défaut
+  status?: TeamStatus;
+}
