2026f70be0ccc232a88111804a0325501c739f28
Kevin SEMITEJA <kevinsemiteja5@gmai.com>
2025-12-28 18:01:04 +0100

    Fix: Add validation decorators to CreateTourClientDto

--- src/tour-clients/dto/create-tour-client.dto.ts
+++ src/tour-clients/dto/create-tour-client.dto.ts
@@ -1,5 +1,16 @@
+import { IsUUID, IsNotEmpty, IsInt, Min } from 'class-validator';
+
 export class CreateTourClientDto {
+  @IsUUID()
+  @IsNotEmpty()
   tourId: string;
+
+  @IsUUID()
+  @IsNotEmpty()
   clientId: string;
+
+  @IsInt()
+  @Min(1)
+  @IsNotEmpty()
   position: number;
 }
