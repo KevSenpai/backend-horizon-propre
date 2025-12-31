1e78b5e1c827474dd0fb850ba902824a1881f7d9
KevSenpai <kevinsemiteja5@gmail.com>
2025-12-31 12:48:52 +0100

    Fix: Allow status update in UpdateInvoiceDto

--- src/invoices/dto/update-invoice.dto.ts
+++ src/invoices/dto/update-invoice.dto.ts
@@ -1,4 +1,11 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateInvoiceDto } from './create-invoice.dto';
+import { IsEnum, IsOptional } from 'class-validator';
+import { InvoiceStatus } from '../entities/invoice.entity';
 
-export class UpdateInvoiceDto extends PartialType(CreateInvoiceDto) {}
+export class UpdateInvoiceDto extends PartialType(CreateInvoiceDto) {
+  // On ajoute explicitement le droit de modifier le statut
+  @IsOptional()
+  @IsEnum(InvoiceStatus)
+  status?: InvoiceStatus;
+}
