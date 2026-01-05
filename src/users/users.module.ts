b50284bf1685df177a59943f8da61b7d6cbd2bfc
KevSenpai <kevinsemiteja5@gmail.com>
2026-01-05 21:02:32 +0100

    Fix: Register User entity in UsersModule

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm'; // <--- Import nécessaire
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { User } from './entities/user.entity'; // <--- Import de l'entité
 
 @Module({
+  // 1. ON ENREGISTRE L'ENTITÉ USER ICI
+  imports: [TypeOrmModule.forFeature([User])], 
+  
   controllers: [UsersController],
   providers: [UsersService],
+  
+  // 2. ON EXPORTE LE SERVICE (Important pour que AuthModule puisse l'utiliser)
+  exports: [UsersService], 
 })
 export class UsersModule {}
