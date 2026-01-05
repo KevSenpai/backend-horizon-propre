11742297371f2b897822188dcead5c29de15fad5
KevSenpai <kevinsemiteja5@gmail.com>
2026-01-05 09:16:50 +0100

    Fix: Add Client entity to CollectionsModule imports

--- src/collections/collections.module.ts
+++ src/collections/collections.module.ts
@@ -3,13 +3,16 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { CollectionsService } from './collections.service';
 import { CollectionsController } from './collections.controller';
 import { Collection } from './entities/collection.entity';
-// AJOUTS IMPORTS
 import { Tour } from '../tours/entities/tour.entity';
 import { TourClient } from '../tour-clients/entities/tour-client.entity';
+// 1. AJOUT DE L'IMPORT MANQUANT
+import { Client } from '../clients/entities/client.entity';
 
 @Module({
-  // AJOUTER Tour ET TourClient DANS LA LISTE
-  imports: [TypeOrmModule.forFeature([Collection, Tour, TourClient])], 
+  imports: [
+    // 2. AJOUT DE Client DANS LA LISTE
+    TypeOrmModule.forFeature([Collection, Tour, TourClient, Client])
+  ],
   controllers: [CollectionsController],
   providers: [CollectionsService],
 })
