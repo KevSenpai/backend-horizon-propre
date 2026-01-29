75866e44a2744eac80b8cbce7a58339bdf9683d5
KevSenpai <kevinsemiteja5@gmail.com>
2026-01-29 21:45:50 +0100

    Fix: Address type errors and chart rendering issues

--- src/clients/dto/update-client.dto.ts
+++ src/clients/dto/update-client.dto.ts
@@ -1,4 +1,14 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateClientDto } from './create-client.dto';
+import { IsOptional, IsObject, Allow } from 'class-validator';
 
-export class UpdateClientDto extends PartialType(CreateClientDto) {}
+export class UpdateClientDto extends PartialType(CreateClientDto) {
+  // @Allow() permet de dire au validateur : "Laisse passer ce champ, quoi qu'il contienne"
+  @IsOptional()
+  @Allow()
+  location?: any;
+
+  @IsOptional()
+  @Allow()
+  location_status?: any;
+}
