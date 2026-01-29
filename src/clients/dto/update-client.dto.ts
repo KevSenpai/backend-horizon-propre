b124f1061e3ee8a473150af15a8313f3624eae54
KevSenpai <kevinsemiteja5@gmail.com>
2026-01-29 21:05:52 +0100

    Fix: Explicitly relax location validation in UpdateClientDto

--- src/clients/dto/update-client.dto.ts
+++ src/clients/dto/update-client.dto.ts
@@ -1,4 +1,9 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateClientDto } from './create-client.dto';
+import { IsOptional } from 'class-validator';
 
-export class UpdateClientDto extends PartialType(CreateClientDto) {}
+export class UpdateClientDto extends PartialType(CreateClientDto) {
+  // On force la permissivité sur la location pour la mise à jour aussi
+  @IsOptional()
+  location?: any;
+}
